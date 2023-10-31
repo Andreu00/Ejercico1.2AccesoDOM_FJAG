b5e8e04d979ac60779cf796dce90a25f0a8051c2
Francisco Javier Andreu García <javiandreugarcia@gmail.com>
2023-10-31 19:56:12 +0100

    Final Exercise

---
+++ AccederConDOM/src/accedercondom/AccederConDOM.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package accedercondom;
+
+import java.io.File;
+
+/**
+ *
+ * @author maget
+ */
+public class AccederConDOM {
+
+    
+    public static void main(String[] args) {
+        AccesoDOM acces=new AccesoDOM();
+        File f=new File("Libros.xml");
+        
+        acces.abrirXMLaDOM(f);
+        
+        acces.recorreDOMyMuestra();
+    }
+    
+}
